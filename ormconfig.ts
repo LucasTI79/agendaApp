0a9f87a49e91adc8c0a8ad3a252f8a5aef216aff
Lucas <lukasalves271@gmail.com>
2021-06-20 17:13:59 -0300

    application local ok

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,12 @@
 module.exports = {
   "type": "postgres",
-  "url": process.env.DATABSE_URL,
   "host": process.env.DB_HOST,
   "port": process.env.DB_PORT,
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_NAME,
-  "migrations": './dist/src/database/migrations/**/*.js',
-  "entities": './dist/src/app/models/**/*.js',
+  "migrations": [process.env.MIGRATIONS],
+  "entities": [process.env.ENTITIES],
   "synchronize": false,
   "logging": false,
   "cli": {
